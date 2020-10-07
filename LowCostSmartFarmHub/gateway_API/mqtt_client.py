1c01009a715e8d86496500ab31a70228d90f726f
itumeleng <itukzz96@gmail.com>
2020-10-07 23:58:21 +0200

    Defined MQTT client class

--- LowCostSmartFarmHub/gateway_API/mqtt_client.py
+++ LowCostSmartFarmHub/gateway_API/mqtt_client.py
@@ -1,26 +1,45 @@
 import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+# To create a MQTT client you must provide broker IP and Port
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("SensorNode1/DHT11")
+class Client:
+    broker_IP:str
+    port:int
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    def __init__(self,broker_IP,port):
+        self.broker_IP=broker_IP
+        self.port=port
+        #mqtt paho-client object 
+        self.client=mqtt.Client()
+        self.client.on_connect = on_connect()
+        self.client.on_message = on_message()
+        self.client.on_publish = on_publish()
+        self.client.connect(self.broker,60)
+        self.client.loop_forever()
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("192.168.0.9", 1883, 60)
+    #The Callback for after connecting to the broker
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("SensorNode1/DHT11")
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(client, userdata, msg):
+        print(msg.topic+" "+str(msg.payload))
+
+    # This callback is called everytime the client publishes a message
+    def on_publish(client,userdata,result)
+        print("Data published ...\n")
+        pass 
+
+    def subscribeToTopic(self,topic:str)
+        self.client.subscribe(topic)
+    
+    def sendMessageToTopic(self,topic,message_payload)
+       self.client.publish(topic,message_payload)
+
 
