7e8edf806693e6d7fd31967557f1cb6e3d24cc81
itumeleng <itukzz96@gmail.com>
2020-10-12 18:40:45 +0200

    Actuator LED function

--- LowCostSmartFarmHub/gateway_API/actuator.py
+++ LowCostSmartFarmHub/gateway_API/actuator.py
@@ -1,5 +1,5 @@
 import time
-from rpi_ws281x import *
+from rpi_ws281x import Color, PixelStrip, ws
 
 class Actuator:
     actuatorName:str
@@ -26,15 +26,19 @@ class Actuator:
         """
         # LED strip configuration:
         LED_COUNT = 32        # Number of LED pixels.
-        LED_PIN = led_pin     # GPIO pin connected to the pixels (must support PWM!).
+        LED_PIN = led_pin          # GPIO pin connected to the pixels (must support PWM!).
         LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-        LED_DMA = 5           # DMA channel to use for generating signal (try 10)
-        LED_BRIGHTNESS = 10   # Set to 0 for darkest and 255 for brightest
+        LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+        LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
         LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+        LED_CHANNEL = 0
+        # LED_STRIP = ws.SK6812_STRIP_RGBW
+        LED_STRIP = ws.SK6812W_STRIP
   
-        # Create PixelStrip object with appropriate configuration.
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+       # Create PixelStrip object with appropriate configuration.
+        strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
         # Intialize the library (must be called once before other functions).
+
         while 1:
             strip.begin()
             #Order
@@ -46,4 +50,4 @@ class Actuator:
                 strip.setPixelColor(i-4,Color(0,0,0))
                 strip.show()
                 time.sleep(0.1)
-
+    
