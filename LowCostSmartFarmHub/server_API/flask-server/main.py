52c9379e40ca0eed46d0444d3f334bc29e7b0580
itumeleng96 <itukzz96@gmail.com>
2020-10-21 22:05:33 +0000

    Added Flask Server

---
+++ LowCostSmartFarmHub/server_API/flask-server/main.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import request
+from flask_httpauth import HTTPBasicAuth
+import json
+from flask_mqtt import Mqtt
+import sys
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+auth = HTTPBasicAuth()
+app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+mqtt = Mqtt(app)
+
+# http basic auth credentials
+users = {
+    "user": "password"
+}
+
+
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
+@app.route('/alert', methods=['POST'])
+@auth.login_required
+def alert():
+#    client.connect("localhost", 1883, 60)
+#    data = json.loads(request.data.decode('utf-8'))
+#    if data['state'] == 'alerting':
+#      client.publish(topic="/alert", payload="1", retain=True)
+#    elif data['state'] == 'ok':
+#       client.publish(topic="/alert", payload="0", retain=True)
+
+#    client.disconnect()
+    return "ok"
+
+@app.route('/control_cmd',methods=['POST','OPTIONS'])
+def control_cmd():
+    #Send ON Command to MQTT Broker
+ 
+    mqtt.publish("cmd/myfarm/dorm-room/gateway/switch/","on",2)
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
