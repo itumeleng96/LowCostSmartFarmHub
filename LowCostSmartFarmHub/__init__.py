f2f3e0c1d1c0c168b3868e4ce9339ce273e26d5b
itumeleng <itukzz96@gmail.com>
2020-10-23 00:13:36 +0200

    Arranging code for publishing to PyPI

--- LowCostSmartFarmHub/__init__.py
+++ LowCostSmartFarmHub/__init__.py
@@ -3,3 +3,11 @@
 __author__ = """Joseph Malemela"""
 __email__ = 'itukzz96@gmail.com'
 __version__ = '0.1.0'
+
+from gateway_API.gateway import Gateway
+from gateway_API.sensor import Sensor
+from gateway_API.actuator import Actuator
+from gateway_API.nodeDevice  import NodeDevice
+
+
+
