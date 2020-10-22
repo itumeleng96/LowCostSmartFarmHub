1206f0c8d6e33791d300d5f4f0a11960d0b083fe
itumeleng <itukzz96@gmail.com>
2020-10-23 00:38:42 +0200

    Fixing Build errors

--- LowCostSmartFarmHub/LowCostSmartFarmHub.py
+++ LowCostSmartFarmHub/LowCostSmartFarmHub.py
@@ -1,5 +1,1 @@
 """Main module."""
-from LowCostSmartFarmHub.gateway import Gateway
-from LowCostSmartFarmHub.sensor import Sensor
-from LowCostSmartFarmHub.actuator import Actuator
-from LowCostSmartFarmHub.nodeDevice  import NodeDevice
