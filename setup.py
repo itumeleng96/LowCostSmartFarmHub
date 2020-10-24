d0826cfbb12d77c492129d4c46804442b4d6b289
Joseph Malemela <37007962+itumeleng96@users.noreply.github.com>
2020-10-24 23:56:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@
 
 from setuptools import setup, find_packages
 
-# this grabs the requirements from requirements.txt
-REQUIREMENTS = [i.strip() for i in open("requirements_dev.txt").readlines()]
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
