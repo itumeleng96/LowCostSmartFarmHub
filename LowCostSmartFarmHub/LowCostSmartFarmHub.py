29433a6c6f7bf003e89a308c4a163860e0c7722b
itumeleng <itukzz96@gmail.com>
2020-10-27 12:59:59 +0200

    docs

--- LowCostSmartFarmHub/LowCostSmartFarmHub.py
+++ LowCostSmartFarmHub/LowCostSmartFarmHub.py
@@ -1,6 +1,1 @@
 """Main module."""
-
-from LowCostSmartFarmHub.sensor import Sensor
-from LowCostSmartFarmHub.actuator import Actuator
-from LowCostSmartFarmHub.gateway import Gateway
-from LowCostSmartFarmHub.nodeDevice import NodeDevice
