21c1a9427f818b8152d75bf3c595427775129b39
itumeleng <itukzz96@gmail.com>
2020-10-27 12:53:42 +0200

    read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import LowCostSmartFarmHub
+#import LowCostSmartFarmHub
 import sphinx_rtd_theme
 
 # -- General configuration ---------------------------------------------
@@ -32,7 +32,7 @@ import sphinx_rtd_theme
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.napoleon','recommonmark','digi-xbee']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.napoleon','recommonmark',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
